994ef30edbbe71cc334358c177ac2e4a9c3bb60f
Erico Andre <ericoandresilva@gmail.com>
2018-12-03 23:26:42 -0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,10 +4,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const configDB = require('./config/database');
-//const cors = require('./cors');
 
 const app = express();
-//app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+})
 
 console.log("app: Carregando Banco de Dados...");
 //Load Database
